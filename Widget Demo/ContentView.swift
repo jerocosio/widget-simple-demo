4ce72e8bd6b775199481907dfce1cc625fb184fa
Jeronimo Cosio <jeronimocosio@gmail.com>
2022-05-03 17:45:27 -0500

    Initial Commit

---
+++ Widget Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Widget Demo
+//
+//  Created by Jeronimo Cosio on 03/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Widget Demo/Widget_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_DemoApp.swift
+//  Widget Demo
+//
+//  Created by Jeronimo Cosio on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Widget_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
